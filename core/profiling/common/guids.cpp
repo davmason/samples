9b77b5457d5bbb41359ad121d5ba823fe14ae852
David Mason <davmason@microsoft.com>
2023-01-11 20:47:48 -0800

    Update guids.cpp

--- core/profiling/common/guids.cpp
+++ core/profiling/common/guids.cpp
@@ -1,6 +1,9 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+#ifndef WIN32
+#include "profiler_pal.h"
+#endif // WIN32
 #include <rpc.h>
 #include <rpcndr.h>
 
