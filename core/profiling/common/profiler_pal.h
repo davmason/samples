88876fa454bcdfbcd877c81e675263a52e8a7d78
David Mason <davmason@microsoft.com>
2023-01-11 20:14:29 -0800

    s

--- core/profiling/common/profiler_pal.h
+++ core/profiling/common/profiler_pal.h
@@ -126,7 +126,23 @@ typedef const GUID *LPCGUID;
 #define GUID_DEFINED
 #endif // !GUID_DEFINED
 
-typedef GUID IID;
+#ifndef __IID_DEFINED__
+#define __IID_DEFINED__
+
+typedef struct _IID
+{
+    unsigned long x;
+    unsigned short s1;
+    unsigned short s2;
+    unsigned char  c[8];
+} IID;
+
+#endif // __IID_DEFINED__
+
+#ifndef CLSID_DEFINED
+#define CLSID_DEFINED
+typedef IID CLSID;
+#endif // CLSID_DEFINED
 
 #define REFGUID const GUID &
 #define REFIID const IID &
